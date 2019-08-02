55cbbe387845108da86f7d97b42bc15738b54d03
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-02 01:08:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -15,7 +15,11 @@ class Shoe
   end
 
   def cobble
+<<<<<<< HEAD
     puts "Your shoe is as good as new!"
+=======
+    puts "Shoe has been repaired"
+>>>>>>> e67c2556302bdd1f2eb4584ea30d94dba2555333
     @condition = "new"
   end
 end
